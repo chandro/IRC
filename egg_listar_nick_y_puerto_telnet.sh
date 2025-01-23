70738fa77adb53b6e95fcaa673282a8900033efa
Alejandro <chandro@users.noreply.github.com>
2025-01-22 20:21:25 -0600

    Update egg_listar_nick_y_puerto_telnet.sh

--- egg_listar_nick_y_puerto_telnet.sh
+++ egg_listar_nick_y_puerto_telnet.sh
@@ -1,10 +1,7 @@
-#!/bin/bash
-
-# Actualizar la base de datos de archivos
-updatedb
+#!/bin/sh
 
 # Buscar archivos de configuración de Eggdrop (.conf) en /home
-config_files=$(locate -r '^/home/.*\.conf$')
+config_files=$(find /home -type f -name "*.conf")
 
 # Crear un archivo temporal para almacenar los resultados
 temp_file=$(mktemp)
@@ -12,13 +9,13 @@ temp_file=$(mktemp)
 # Procesar cada archivo de configuración encontrado
 for config_file in $config_files; do
     # Obtener el puerto Telnet
-    telnet_port=$(grep -E '^\s{0,3}listen ' "$config_file" | grep -v '#' | awk '{print $2}')
-    
+    telnet_port=$(grep -E '^\s*listen ' "$config_file" | grep -v '#' | awk '{print $2}')
+
     # Obtener el nick
     nick=$(grep -E '^set nick "' "$config_file" | awk -F'"' '{print $2}')
-    
+
     # Si ambos valores se encontraron, escribirlos en el archivo temporal
-    if [[ -n $telnet_port && -n $nick ]]; then
+    if [ -n "$telnet_port" ] && [ -n "$nick" ]; then
         echo "nick $nick telnet port $telnet_port" >> "$temp_file"
     fi
 done
